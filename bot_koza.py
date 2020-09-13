066e8af78d746a3a803c30f55588cc879e9de6ad
Valeriy Mirancev <azerus21@gmail.com>
2020-09-13 17:26:27 +0700

    Update bot_koza.py

--- bot_koza.py
+++ bot_koza.py
@@ -1,51 +1,16 @@
 import discord
-import profanity_filter
+from discord.ext import commands
 import os
 
-PREFIX = '!'
-
-
-def read_token():
-    with open("token.txt", "r") as f:
-        lines = f.readlines()
-        return lines[0].strip()
-
-
-def check_message(message):
-    msg = message.content.lower()
-
-    for word in profanity_filter.bad_words:
-        if msg.find(word) != -1:
-            return True
-
-    return False
-
+Bot = commands.Bot(command_prefix="!")
+Bot.remove_command('help')
 
 # token = read_token()
 token = str(os.environ.get('BOT_TOKEN'))
 
-client = discord.Client()
 
-
-@client.event
+@Bot.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('Наблюдаю за тобой!'), )
+    await Bot.change_presence(status=discord.Status.online, activity=discord.Game('Наблюдаю за тобой!'), )
 
-
-@client.event
-async def on_message(message):
-    channels = ["загон"]
-
-    # profanity_filter
-    bot_react = check_message(message)
-
-    if bot_react:
-        await message.delete()
-        await message.channel.send("AAAAAAAAAAAAAA!")
-
-    # commands
-    if str(message.channel) in channels:
-        if message.content.find("коза") != -1:
-            await message.channel.send("AAAAAAAAAAAAAA!")
-
-client.run(token)
+Bot.run(token)
