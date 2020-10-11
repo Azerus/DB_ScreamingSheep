6181c4b165e3bdba76f2aaa3d35459d59fb45013
Valeriy Mirancev <azerus21@gmail.com>
2020-10-11 14:16:39 +0700

    Update bot_koza.py

--- bot_koza.py
+++ bot_koza.py
@@ -2,10 +2,14 @@ import discord
 import profanity_filter
 import os
 import time
+from discord.ext import commands
+import random
+import youtube_dl
 import koza_settings
 
 
-PREFIX = "!"
+token = str(os.environ.get('BOT_TOKEN'))
+client = commands.Bot(command_prefix = '!')
 
 
 def check_message(msg):
@@ -16,21 +20,20 @@ def check_message(msg):
     return False
 
 
-# token = read_token()
-token = str(os.environ.get('BOT_TOKEN'))
-
-client = discord.Client()
+def get_log_channel(name):
+    for channel in client.get_guild(int(os.environ.get('SERVER_ID'))).channels:
+        if channel.name == name:
+            return channel
 
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('Наблюдаю за тобой!'), )
+    status = koza_settings.statuses[1]
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(status), )
 
 
 @client.event
 async def on_message(message):
-    channels = ["загон"]
-
     msg = message.content.lower()
 
     # profanity_filter
@@ -43,6 +46,8 @@ async def on_message(message):
                 delete = False
 
         if delete:
+            log_channel = get_log_channel(koza_settings.log_channel)
+            await log_channel.send(message.author.name + ": " + msg)
             await message.delete()
             time.sleep(1)
             await message.channel.send("AAAAAAAAAAAAAA!")
@@ -50,9 +55,9 @@ async def on_message(message):
 
     # commands
     if str(message.channel) in koza_settings.command_channel:
-        if msg == '{}help'.format(PREFIX):
+        if msg == '{}help'.format(koza_settings.PREFIX):
             await command_help(message.channel)
-        elif msg == '{}users'.format(PREFIX):
+        elif msg == '{}users'.format(koza_settings.PREFIX):
             await command_users(message.channel)
         elif "загон" not in [y.name.lower() for y in message.author.roles]:
             await message.delete()
@@ -63,7 +68,7 @@ async def on_message(message):
 
 async def command_help(channel):
     emb = discord.Embed(title='Навигация по командам')
-    emb.add_field(name='{}help'.format(PREFIX), value='Список команд')
+    emb.add_field(name='{}help'.format(koza_settings.PREFIX), value='Список команд')
     await channel.send(embed=emb)
 
 
@@ -72,5 +77,6 @@ async def command_users(channel):
     # emb.add_field(name='{}help'.format(PREFIX), value='Список команд')
     server_id = client.get_guild(int(os.environ.get('SERVER_ID')))
     await channel.send(f"""Количество пользователей: {server_id.member_count}""")
+
 
 client.run(token)
