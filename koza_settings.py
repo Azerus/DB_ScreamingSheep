fbdea065cf1ece9300c8e46a491890afe829eaad
Valeriy Mirancev <azerus21@gmail.com>
2020-10-11 14:15:33 +0700

    Update koza_settings.py

--- koza_settings.py
+++ koza_settings.py
@@ -1,3 +1,7 @@
 profanity_ignore_groups = ["загон", "владыка мира нексуса"]
 non_user_channels = ["заходившие"]
 command_channel = ["загон"]
+music_channel = ["KozaDJ"]
+PREFIX = "!"
+statuses = ["Смотрит трансляцию Blizzcon 2021", "Heroes of the Storm", "Наблюдаю за тобой"]
+log_channel = "логи-бота"
