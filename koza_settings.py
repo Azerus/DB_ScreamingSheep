d7ad0a7a3cb7ef9e5a82d24e1b6d232156ab8771
Valeriy Mirancev <azerus21@gmail.com>
2020-10-24 18:30:42 +0700

    Update koza_settings.py

--- koza_settings.py
+++ koza_settings.py
@@ -1,10 +1,12 @@
 profanity_ignore_groups = ["загон", "владыка мира нексуса"]
+moderation_groups = ["загон", "владыка мира нексуса"]
 non_user_channels = ["заходившие"]
 command_channel = ["загон"]
 music_channel = ["KozaDJ"]
 log_channel = "логи-бота"
 PREFIX = "!"
 games = ["Heroes of the Storm", "Skyrim: Goat Edition", "Warcraft 3: Refunded", "сыщика", "грязные игры"]
-watching = ["на тебя!", "стрим HappyDerg", "Netflix", "акции Activision Blizzard", "темы на форуме"]
+watching = ["на тебя!", "стрим HappyDerg", "Netflix", "акции Activision Blizzard", "темы на форуме",
+            "фильм Паршивая овца"]
 blizzcon = "трансляцию Blizzcon 2021"
 listening = ["Авторадио", "Хард Рок", "музыку в Spotify", "сплетни бабки"]
