d900a872f252232dd81c5672449132a01779cd30
Valeriy Mirancev <azerus21@gmail.com>
2020-11-01 17:41:30 +0700

    Update koza_settings.py

--- koza_settings.py
+++ koza_settings.py
@@ -6,9 +6,30 @@ command_channel = ["загон"]
 music_channel = ["KozaDJ"]
 log_channel = "логи-бота"
 PREFIX = "!"
-games = ["Heroes of the Storm", "Skyrim: Goat Edition", "Warcraft 3: Refunded", "сыщика", "грязные игры"]
-watching = ["на тебя!", "стрим HappyDerg", "Netflix", "акции Activision Blizzard", "темы на форуме",
+games = ["Heroes of the Storm",
+         "Skyrim: Goat Edition",
+         "Warcraft 3: Refunded",
+         "сыщика",
+         "грязные игры"]
+watching = ["на тебя!",
+            "стрим HappyDerg",
+            "Netflix",
+            "акции Activision Blizzard",
+            "темы на форуме",
             "фильм Паршивая овца"]
-blizzcon = "трансляцию Blizzcon 2021"
-special = "The Dark Pictures Anthology: Little Hope"
-listening = ["Авторадио", "Хард Рок", "музыку в Spotify", "сплетни бабки"]
+special = "трансляцию Blizzcon 2021"
+listening = ["Авторадио",
+             "Хард Рок",
+             "музыку в Spotify",
+             "сплетни бабки"]
+reaction = ["Коза хочет прописать в табло {}",
+            "Коза с подозрением смотрит на {}",
+            "Коза бьет по земле копытами и хочет боднуть {}",
+            "Коза хочет поорать с {}",
+            "Коза выехала на дом к {}",
+            "Коза хочет в отпуск подальше от {}",
+            "Коза не понимает {}",
+            "Коза, одетая в скафандр, парит в открытом космосе и ее крик слышит {}",
+            "Коза орет над {}",
+            "Коза хочет чтобы {} не беспокоил ее",
+            "Коза улетела на орбиту планеты Плутон от {}"]
