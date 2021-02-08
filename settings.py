2a0cccb821c192ad22823b0ca83e62cc2bf2cf20
Valeriy Mirancev <azerus21@gmail.com>
2021-02-09 01:43:54 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-KOZA_PREFIX = "!"
-AMADEUS_PREFIX = "$"
+KOZA_PREFIX = "."
+AMADEUS_PREFIX = "!"
 log_channel = "логи-ботов"
-command_channel = "загон"
+command_channel = "консоль"
 moderation_groups = ["загон", "владыки миров нексуса", "хозяин таверны", "ии"]
