07c1e32e2848ee6114e31a1ba25e52a3f49404a2
Valeriy Mirancev <azerus21@gmail.com>
2021-10-07 22:08:43 +0700

    Create settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 KOZA_PREFIX = "."
 AMADEUS_PREFIX = "!"
 log_channel = "логи-ботов"
+pda_channel = "новости-4pda"
 command_channel = "консоль"
 moderation_groups = ["загон", "владыки миров нексуса", "хозяин таверны", "ии"]
